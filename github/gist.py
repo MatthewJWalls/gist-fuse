6b255c58e178ab66ec5e4e053fcb9518dca51404
Matthew Walls <matthewjwalls@googlemail.com>
2015-10-05 13:17:03 -0700

    Implement getting Gist content on lazy load

--- github/gist.py
+++ github/gist.py
@@ -3,6 +3,20 @@ API_BASE_URL = "https://api.github.com"
 
 import requests
 
+class GistProxy:
+
+    """ lazy-loaded proxy for the gist file data """
+    
+    def __init__(self, gistFileDict):
+        """ takes a "files" dict from the json repr """
+        self.name = gistFileDict["filename"]
+        self.url = gistFileDict["raw_url"]
+
+    def content(self):
+        """ returns the content of the gist """
+        r = requests.get(self.url)
+        return r.text
+        
 class API:
 
     """ thin requests wrapper around the github gist API """
@@ -18,4 +32,12 @@ class API:
     def all(self):
         """ retrieve a list of all gist names """
         g = requests.get("%s/%s" % (API_BASE_URL, self.endpoints["list"]))
-        return g.json()
+
+        files = []
+        
+        for gist in g.json():
+            for gistFile in gist["files"].values():
+                files.append(GistProxy(gistFile))
+
+        return files
+    
--- tests.py
+++ tests.py
@@ -10,9 +10,14 @@ class IntegrationTests(unittest.TestCase):
     """ Integration tests that exercises the system """
     
     def test_get_all(self):
-        """ test the stub """
+        """ test getting a list of gists """
         api = gist.API(name=TEST_USER)
         self.assertTrue(len(api.all()) > 0)
+
+    def test_get_content(self):
+        """ test getting the contents of a gist """
+        api = gist.API(name=TEST_USER)
+        self.assertTrue(len(api.all()[0].content()) > 0)
 
 if __name__ == "__main__":
     unittest.main()

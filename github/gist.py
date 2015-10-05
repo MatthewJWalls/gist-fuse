94866565ea929286d5fc2104f35f8751af573ffd
Matthew Walls <matthewjwalls@googlemail.com>
2015-10-05 13:00:03 -0700

    Stub in a little get-all-gists functionality

--- github/gist.py
+++ github/gist.py
@@ -1,5 +1,21 @@
 
+API_BASE_URL = "https://api.github.com"
+
+import requests
+
 class API:
 
-    def stub(self):
-        return "stub"
+    """ thin requests wrapper around the github gist API """
+
+    def __init__(self, name=None, token=None):
+        """ we take the users github api token in the constructor """
+        self.token = token
+        self.name = name
+        self.endpoints = {
+            "list" : "users/%s/gists" % name
+        }
+
+    def all(self):
+        """ retrieve a list of all gist names """
+        g = requests.get("%s/%s" % (API_BASE_URL, self.endpoints["list"]))
+        return g.json()
--- tests.py
+++ tests.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 import unittest
-
 from github import gist
 
-class TestGistAPI(unittest.TestCase):
+TEST_USER = "mojombo" # github's CEO
 
-    def test_stub(self):
+class IntegrationTests(unittest.TestCase):
+
+    """ Integration tests that exercises the system """
+    
+    def test_get_all(self):
         """ test the stub """
-        api = gist.API()
-        self.assertEqual(api.stub(), "stub")
+        api = gist.API(name=TEST_USER)
+        self.assertTrue(len(api.all()) > 0)
 
 if __name__ == "__main__":
     unittest.main()

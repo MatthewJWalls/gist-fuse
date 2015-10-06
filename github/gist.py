bdf1d0a25e23b88c291a11815fdd32ef3e09937c
Matthew Walls <matthewjwalls@googlemail.com>
2015-10-06 00:19:39 -0700

    Use requests session object in gist api

--- github/gist.py
+++ github/gist.py
@@ -1,32 +1,31 @@
 
-API_BASE_URL = "https://api.github.com"
-
 import requests
 import json
+
+API_BASE_URL = "https://api.github.com"
 
 class GistProxy:
 
     """ lazy-loaded proxy for the gist file data """
     
-    def __init__(self, headers, api, gistFileDict):
+    def __init__(self, session, api, gistFileDict):
         """ takes a "files" dict from the json repr """
-        self.headers = headers
+        self.session = session
         self.api = api
         self.name = gistFileDict["filename"]
         self.url = gistFileDict["raw_url"]
 
     def content(self):
         """ returns the content of the gist """
-        r = requests.get(self.url)
+        r = self.session.get(self.url)
         return r.text
 
     def update(self, data):
         """ replace the content of this gist with data """
         content = { "files" : { self.name : { "content" : data } }}
 
-        r = requests.patch(
+        r = self.session.patch(
             self.api,
-            headers=self.headers,
             data=json.dumps(content)
         )
     
@@ -36,9 +35,12 @@ class API:
 
     def __init__(self, name=None, token=None):
         """ we take the users github api token in the constructor """
-        self.token = token
         self.name = name
-        self.headers = {"Authorization" : "token %s" % token}
+        self.token = token
+        self.session = requests.Session()
+        self.session.headers.update({"Authorization" : "token %s" % token})
+        self.session.headers.update({"Connection" : "close"})
+        
         self.endpoints = {
             "list" : "users/%s/gists" % name
         }
@@ -46,9 +48,8 @@ class API:
     def all(self):
         """ retrieve a list of all gist names """
 
-        g = requests.get(
+        g = self.session.get(
             "%s/%s" % (API_BASE_URL, self.endpoints["list"]),
-            headers=self.headers
         )
 
         files = []
@@ -56,7 +57,7 @@ class API:
         for gist in g.json():
             for gistFile in gist["files"].values():
                 files.append(GistProxy(
-                    self.headers,
+                    self.session,
                     gist["url"],
                     gistFile
                 ))
